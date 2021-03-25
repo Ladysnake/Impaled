61c00bb378cc0e0d508196a6b19f0c89ef909096
doctor4t <25477005+doctor4t@users.noreply.github.com>
2021-03-25 20:36:19 +0100

    Custom atlan entity renderer

--- src/main/java/ladysnake/impaled/mixin/TridentRiptideFeatureRendererMixin.java
+++ src/main/java/ladysnake/impaled/mixin/TridentRiptideFeatureRendererMixin.java
@@ -1,6 +1,7 @@
 package ladysnake.impaled.mixin;
 
 import ladysnake.impaled.common.init.ImpaledItems;
+import ladysnake.sincereloyalty.SincereLoyalty;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
@@ -19,7 +20,7 @@ import static ladysnake.impaled.client.ImpaledClient.HELLFORK_RIPTIDE_TEXTURE;
 public abstract class TridentRiptideFeatureRendererMixin {
     @ModifyVariable(method = "render", at = @At("STORE"))
     private VertexConsumer swapHotRiptide(VertexConsumer orig, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int light, LivingEntity livingEntity) {
-        if (livingEntity instanceof PlayerEntity && livingEntity.isUsingRiptide() && (livingEntity.getMainHandStack().getItem() == ImpaledItems.HELLFORK || (livingEntity.getOffHandStack().getItem() == ImpaledItems.HELLFORK) && !(livingEntity.getMainHandStack().getItem() instanceof TridentItem))) {
+        if (livingEntity instanceof PlayerEntity && livingEntity.isUsingRiptide() && (livingEntity.getMainHandStack().getItem() == ImpaledItems.HELLFORK || (livingEntity.getOffHandStack().getItem() == ImpaledItems.HELLFORK) && !(SincereLoyalty.TRIDENTS.contains(livingEntity.getMainHandStack().getItem())))) {
             return vertexConsumerProvider.getBuffer(RenderLayer.getEntityCutoutNoCull(HELLFORK_RIPTIDE_TEXTURE));
         }
         return orig;
