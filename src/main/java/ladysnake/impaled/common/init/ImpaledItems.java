b708adf640d06ba426a985d8b3803a94853b5882
doctor4t <25477005+doctor4t@users.noreply.github.com>
2021-03-25 14:29:26 +0100

    Add maelstrom

--- src/main/java/ladysnake/impaled/common/init/ImpaledItems.java
+++ src/main/java/ladysnake/impaled/common/init/ImpaledItems.java
@@ -5,6 +5,7 @@ import ladysnake.impaled.common.Impaled;
 import ladysnake.impaled.common.entity.ImpaledTridentEntity;
 import ladysnake.impaled.common.item.HellforkItem;
 import ladysnake.impaled.common.item.ImpaledTridentItem;
+import ladysnake.impaled.common.item.MaelstromItem;
 import ladysnake.impaled.common.item.PitchforkItem;
 import net.minecraft.block.DispenserBlock;
 import net.minecraft.block.dispenser.ProjectileDispenserBehavior;
@@ -23,12 +24,14 @@ public class ImpaledItems {
     public static Item PITCHFORK;
     public static Item HELLFORK;
     public static Item ELDER_TRIDENT;
+    public static Item PHANTASM;
     public static final Set<ImpaledTridentItem> ALL_TRIDENTS = new ReferenceOpenHashSet<>();
 
     public static void init() {
         PITCHFORK = registerTrident(new PitchforkItem((new Item.Settings()).maxDamage(250).group(ItemGroup.COMBAT), ImpaledEntityTypes.PITCHFORK), "pitchfork", true);
         HELLFORK = registerTrident(new HellforkItem((new Item.Settings()).maxDamage(325).fireproof().group(ItemGroup.COMBAT), ImpaledEntityTypes.HELLFORK), "hellfork", true);
         ELDER_TRIDENT = registerTrident(new ImpaledTridentItem((new Item.Settings()).maxDamage(250).group(ItemGroup.COMBAT), ImpaledEntityTypes.ELDER_TRIDENT), "elder_trident", true);
+        PHANTASM = registerItem(new MaelstromItem((new Item.Settings()).maxDamage(80).group(ItemGroup.COMBAT)), "maelstrom");
     }
 
     public static ImpaledTridentItem registerTrident(ImpaledTridentItem item, String name, boolean registerDispenserBehavior) {
@@ -49,4 +52,8 @@ public class ImpaledItems {
         return item;
     }
 
+    public static Item registerItem(Item item, String name) {
+        Registry.register(Registry.ITEM, Impaled.MODID + ":" + name, item);
+        return item;
+    }
 }
---
+++ src/main/java/ladysnake/impaled/common/item/MaelstromItem.java
@@ -0,0 +1,101 @@
+package ladysnake.impaled.common.item;
+
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.entity.projectile.TridentEntity;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.RangedWeaponItem;
+import net.minecraft.item.TridentItem;
+import net.minecraft.item.Vanishable;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
+import net.minecraft.world.World;
+
+import java.util.function.Predicate;
+
+public class MaelstromItem extends RangedWeaponItem implements Vanishable {
+    public MaelstromItem(Item.Settings settings) {
+        super(settings);
+    }
+
+    public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks) {
+        if (user instanceof PlayerEntity) {
+            ((PlayerEntity) user).getItemCooldownManager().set(this, 20-(3*EnchantmentHelper.getLevel(Enchantments.EFFICIENCY, stack)));
+        }
+    }
+
+    public int getMaxUseTime(ItemStack stack) {
+        return 72000;
+    }
+
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.BLOCK;
+    }
+
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack itemStack = user.getStackInHand(hand);
+        user.setCurrentHand(hand);
+        return TypedActionResult.consume(itemStack);
+    }
+
+    @Override
+    public void usageTick(World world, LivingEntity user, ItemStack stack, int remainingUseTicks) {
+        super.usageTick(world, user, stack, remainingUseTicks);
+        if (!user.isSneaking() && remainingUseTicks % (20-(3*EnchantmentHelper.getLevel(Enchantments.EFFICIENCY, stack))) == 0 && world instanceof ServerWorld) {
+            if (user instanceof PlayerEntity) {
+                Inventory inventory = ((PlayerEntity) user).getInventory();
+                for (int i = 0; i < inventory.size(); i++) {
+                    ItemStack itemStack = ((PlayerEntity) user).getInventory().getStack(i);
+                    if (!itemStack.isEmpty() && EnchantmentHelper.getRiptide(itemStack) == 0) {
+                        TridentEntity trident = null;
+                        PlayerEntity playerEntity = (PlayerEntity)user;
+
+                        if (itemStack.getItem() instanceof ImpaledTridentItem) {
+                            trident = ((ImpaledTridentItem) itemStack.getItem()).createTrident(world, user, itemStack);
+                        } else if (itemStack.getItem() instanceof TridentItem) {
+                            trident = new TridentEntity(world, user, itemStack);
+                            trident.setProperties(playerEntity, playerEntity.pitch, playerEntity.yaw, 0.0F, 2.5F, 1.0F);
+                        }
+
+                        if (trident != null) {
+                            itemStack.damage(1, (LivingEntity) playerEntity, livingEntity -> livingEntity.sendToolBreakStatus(user.getActiveHand()));
+                            stack.damage(1, (LivingEntity) playerEntity, livingEntity -> livingEntity.sendToolBreakStatus(user.getActiveHand()));
+
+
+                            if (playerEntity.getAbilities().creativeMode) {
+                                trident.pickupType = PersistentProjectileEntity.PickupPermission.CREATIVE_ONLY;
+                            }
+
+                            world.spawnEntity(trident);
+                            world.playSoundFromEntity(null, playerEntity, SoundEvents.ITEM_TRIDENT_RETURN, SoundCategory.PLAYERS, 1.0F, 1.0F);
+                            if (!playerEntity.getAbilities().creativeMode) {
+                                playerEntity.getInventory().removeOne(itemStack);
+                            }
+
+                            playerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public Predicate<ItemStack> getProjectiles() {
+        return itemStack -> itemStack.getItem() instanceof TridentItem;
+    }
+
+    public int getRange() {
+        return 15;
+    }
+}
