2ecdad4c27a9ec951f5a3e38eef747cfc13988c3
doctor4t <25477005+doctor4t@users.noreply.github.com>
2021-04-10 16:57:26 +0200

    Add water bubble particle effects to the hellfork

--- src/main/java/ladysnake/impaled/common/entity/HellforkEntity.java
+++ src/main/java/ladysnake/impaled/common/entity/HellforkEntity.java
@@ -1,6 +1,8 @@
 package ladysnake.impaled.common.entity;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.world.World;
 
@@ -23,5 +25,14 @@ public class HellforkEntity extends ImpaledTridentEntity {
     @Override
     public boolean doesRenderOnFire() {
         return false;
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+
+        if (this.isSubmergedInWater() && this.world.isClient() && this.random.nextInt(5) == 0) {
+            this.world.addParticle(ParticleTypes.BUBBLE_COLUMN_UP, this.getX()+random.nextGaussian()/10, this.getY()+random.nextGaussian()/10, this.getZ()+random.nextGaussian()/10, 0, this.random.nextFloat(), 0);
+        }
     }
 }
